3f6d0885743c881d736e5c20266821c32d66da2b
Javier Leyes <javier.nunezleyes@gmail.com>
2024-11-11 22:36:55 -0300

    Refactor Program.cs and add CosmosExplorer.Core project
    Updated dev environment.md to include Visual Studio 2022 or VS Code as requirements. Modified CosmosExplorer.Terminal.csproj to add references to Newtonsoft.Json and CosmosExplorer.Core.csproj. Refactored Program.cs to use CosmosExplorerHelper from CosmosExplorer.Core, including replacing direct usage of CosmosClient, changing ConnectionString to CONNECTION_STRING, and removing several methods. Updated CosmosExplorer.sln to include CosmosExplorer.Core project. Added new CosmosExplorer.Core.csproj with references to Microsoft.Azure.Cosmos and Newtonsoft.Json. Added CosmosExplorerHelper class to encapsulate CosmosClient logic.

--- src/CosmosExplorer.Terminal/Program.cs
+++ src/CosmosExplorer.Terminal/Program.cs
@@ -1,16 +1,17 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using CosmosExplorer.Core;
+using Microsoft.Azure.Cosmos;
 
 class Program
 {
-    private const string ConnectionString = "AccountEndpoint=https://host.docker.internal:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+    private const string CONNECTION_STRING = "AccountEndpoint=https://host.docker.internal:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
     static async Task Main(string[] args)
     {
-        // Create Cosmos Client.
-        CosmosClient cosmosClient = CreateCosmosClient();
+        // Create a new instance of the CosmosClient class.
+        CosmosExplorerHelper cosmosExplorerHelper = new CosmosExplorerHelper(CONNECTION_STRING);
 
         // Create an iterator to list the databases.
-        FeedIterator<DatabaseProperties> iterator = GetDatabaseIterator(cosmosClient);
+        FeedIterator<DatabaseProperties> iterator = cosmosExplorerHelper.GetDatabaseIterator();
 
         // TODO: Remove this variable, just for test.
         string databaseName = string.Empty;
@@ -18,8 +19,8 @@ class Program
         // Iterate through the databases and print their names.
         while (iterator.HasMoreResults)
         {
-            FeedResponse<DatabaseProperties> response = await iterator.ReadNextAsync();
-            foreach (var database in response)
+            FeedResponse<DatabaseProperties> databases = await iterator.ReadNextAsync();
+            foreach (var database in databases)
             {
                 databaseName = database.Id;
                 Console.WriteLine(database.Id);
@@ -27,7 +28,7 @@ class Program
         }
 
         // Create an iterator to list the containers in the current database.
-        FeedIterator<ContainerProperties> containerIterator = GetContainerIterator(cosmosClient, databaseName);
+        FeedIterator<ContainerProperties> containerIterator = cosmosExplorerHelper.GetContainerIterator(databaseName);
 
         // Iterate through the containers and print their names.
         while (containerIterator.HasMoreResults)
@@ -41,12 +42,12 @@ class Program
 
         // Run a query in a specific container.
         databaseName = "Oversight";
-        string containerName = "Project"; 
-        Container containerQuery = cosmosClient.GetContainer(databaseName, containerName);
+        string containerName = "Project";
 
         // Define the query
         string query = "SELECT * FROM c WHERE c.ProjectId = '45345345asdfs'"; // Replace with your query
-        FeedIterator<dynamic> queryIterator = containerQuery.GetItemQueryIterator<dynamic>(new QueryDefinition(query));
+
+        FeedIterator<dynamic> queryIterator = cosmosExplorerHelper.GetQueryIterator(databaseName, containerName, query);
 
         // Iterate through the query results and print them
         while (queryIterator.HasMoreResults)
@@ -57,61 +58,5 @@ class Program
                 Console.WriteLine(item);
             }
         }
-    }
-
-    /// <summary>
-    /// Creates a new instance of the <see cref="CosmosClient"/> class with custom options.
-    /// This method configures the client to accept self-signed certificates in development.
-    /// </summary>
-    /// <returns>A configured <see cref="CosmosClient"/> instance.</returns>
-    private static CosmosClient CreateCosmosClient()
-    {
-        // This is a workaround to accept self-signed certificates in development.
-        CosmosClientOptions options = new()
-        {
-            HttpClientFactory = () => new HttpClient(new HttpClientHandler()
-            {
-                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-            }),
-            ConnectionMode = ConnectionMode.Gateway,
-        };
-
-        return new CosmosClient(ConnectionString, clientOptions: options);
-    }
-
-    /// <summary>
-    /// Gets an iterator to list the databases in the Cosmos DB account.
-    /// </summary>
-    /// <param name="cosmosClient">The <see cref="CosmosClient"/> instance to use for querying databases.</param>
-    /// <returns>A <see cref="FeedIterator{DatabaseProperties}"/> to iterate through the databases.</returns>
-    private static FeedIterator<DatabaseProperties> GetDatabaseIterator(CosmosClient cosmosClient)
-    {
-        return cosmosClient.GetDatabaseQueryIterator<DatabaseProperties>();
-    }
-
-    /// <summary>
-    /// Gets an iterator to list the containers in a specific database.
-    /// </summary>
-    /// <param name="cosmosClient">The <see cref="CosmosClient"/> instance to use for querying containers.</param>
-    /// <param name="databaseName">The name of the database to list containers from.</param>
-    /// <returns>A <see cref="FeedIterator{ContainerProperties}"/> to iterate through the containers.</returns>
-    private static FeedIterator<ContainerProperties> GetContainerIterator(CosmosClient cosmosClient, string databaseName)
-    {
-        Database database = cosmosClient.GetDatabase(databaseName);
-        return database.GetContainerQueryIterator<ContainerProperties>();
-    }
-
-    /// <summary>
-    /// Gets an iterator to run a query in a specific container.
-    /// </summary>
-    /// <param name="cosmosClient">The <see cref="CosmosClient"/> instance to use for querying items.</param>
-    /// <param name="databaseName">The name of the database containing the container.</param>
-    /// <param name="containerName">The name of the container to run the query against.</param>
-    /// <param name="query">The query to execute.</param>
-    /// <returns>A <see cref="FeedIterator{T}"/> to iterate through the query results.</returns>
-    private static FeedIterator<dynamic> GetQueryIterator(CosmosClient cosmosClient, string databaseName, string containerName, string query)
-    {
-        Container containerQuery = cosmosClient.GetContainer(databaseName, containerName);
-        return containerQuery.GetItemQueryIterator<dynamic>(new QueryDefinition(query));
     }
 }
---
+++ src/CosmosExplorer/CosmosExplorerHelper.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace CosmosExplorer.Core
+{
+    public class CosmosExplorerHelper
+    {
+        private readonly CosmosClient _cosmosClient;
+
+        public CosmosExplorerHelper(string connectionString)
+        {
+            _cosmosClient = CreateCosmosClient(connectionString);
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="CosmosClient"/> class with the specified connection string.
+        /// This method configures the client to accept self-signed certificates in development environments.
+        /// </summary>
+        /// <param name="connectionString">The connection string to use for the Cosmos DB account.</param>
+        /// <returns>A new instance of the <see cref="CosmosClient"/> class.</returns>
+        public CosmosClient CreateCosmosClient(string connectionString)
+        {
+            // This is a workaround to accept self-signed certificates in development.
+            CosmosClientOptions options = new()
+            {
+                HttpClientFactory = () => new HttpClient(new HttpClientHandler()
+                {
+                    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                }),
+                ConnectionMode = ConnectionMode.Gateway,
+            };
+
+            return new CosmosClient(connectionString, clientOptions: options);
+        }
+
+        /// <summary>
+        /// Gets an iterator to list the databases in the Cosmos DB account.
+        /// </summary>
+        /// <returns>A <see cref="FeedIterator{DatabaseProperties}"/> to iterate through the databases.</returns>
+        public FeedIterator<DatabaseProperties> GetDatabaseIterator()
+        {
+            return _cosmosClient.GetDatabaseQueryIterator<DatabaseProperties>();
+        }
+
+        /// <summary>
+        /// Gets an iterator to list the containers in a specific database.
+        /// </summary>
+        /// <param name="databaseName">The name of the database to list containers from.</param>
+        /// <returns>A <see cref="FeedIterator{ContainerProperties}"/> to iterate through the containers.</returns>
+        public FeedIterator<ContainerProperties> GetContainerIterator(string databaseName)
+        {
+            Database database = _cosmosClient.GetDatabase(databaseName);
+            return database.GetContainerQueryIterator<ContainerProperties>();
+        }
+
+        /// <summary>
+        /// Gets an iterator to run a query in a specific container.
+        /// </summary>
+        /// <param name="databaseName">The name of the database containing the container.</param>
+        /// <param name="containerName">The name of the container to run the query against.</param>
+        /// <param name="query">The query to execute.</param>
+        /// <returns>A <see cref="FeedIterator{T}"/> to iterate through the query results.</returns>
+        public FeedIterator<dynamic> GetQueryIterator(string databaseName, string containerName, string query)
+        {
+            Container containerQuery = _cosmosClient.GetContainer(databaseName, containerName);
+            return containerQuery.GetItemQueryIterator<dynamic>(new QueryDefinition(query));
+        }
+    }
+}
